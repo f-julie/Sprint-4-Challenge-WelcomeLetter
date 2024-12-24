aa41d9e8d8c00bd49623f8e28907b055376fdeed
Julie Fiess <juliefiess@gmail.com>
2024-12-24 11:38:37 -0600

    FormLetterService complete

--- src/com/bloomtech/welcomeletter/services/FormLetterService.java
+++ src/com/bloomtech/welcomeletter/services/FormLetterService.java
@@ -8,11 +8,31 @@ import java.io.IOException;
 public class FormLetterService {
     private static final FileManager fileManager = new FileManager();
 
+    // With help from BloomTech Learner Bot
+    private static final String TEMPLATE_PATH = "LetterTemplate";
+
     public void generateWelcomeLetter(Employee employee) throws IOException {
-        //TODO: Get template text from resources
+        // Get template text from resources
+        String template = fileManager.getTextFromFile(TEMPLATE_PATH);
 
-        //TODO: Replace []ed text with correct info
+        // Replace []ed text with correct info
+        String letter = replacePlaceholders(template, employee);
 
-        //TODO: Write finalized String to file
+        // Write finalized String to file
+        fileManager.writeTextToFile("Welcome" + employee.getFirstname() + employee.getLastname(), letter);
+    }
+
+    private String replacePlaceholders(String template, Employee employee) {
+        return template.replace("[fullname]", employee.getFirstname() + " " + employee.getLastname())
+                //.replace("[last_name]", employee.getLastname())
+                //.replace("[phone_number]", employee.getPhonenumber())
+                //.replace("[email]", employee.getEmail())
+                .replace("[startingsalary]", Integer.toString(employee.getSalary()))
+                .replace("[startdate]", employee.getStartdate().toString())
+                //.replace("[role]", employee.getRole().toString())
+                .replace("[company starttime]", employee.getCompany().getCompanyname())
+                .replace("[company name]", employee.getCompany().getCompanyname());
+                //.replace("[id]", employee.getId().toString);
+
     }
 }
