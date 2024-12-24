bb4d1ce1824c1b3ae87dabbd89253bf21cca2dd5
Julie Fiess <juliefiess@gmail.com>
2024-12-24 11:34:10 -0600

    Company completed

--- src/com/bloomtech/welcomeletter/models/Company.java
+++ src/com/bloomtech/welcomeletter/models/Company.java
@@ -7,19 +7,50 @@ public class Company {
     private String companyname;
     private String country;
 
+    public static CompanyBuilder builder() { return new CompanyBuilder(); }
+
     //TODO: Replace with Builder Constructor
-    public Company(String companyname, String country) {
+    private Company(CompanyBuilder builder) { //String companyname, String country
         //Auto-generate an id
         id = maxId;
         maxId++;
 
         //Initialize fields
-        this.companyname = companyname;
-        this.country = country;
+        this.companyname = builder.companyname;
+        this.country = builder.country;
     }
 
-    public static final class Builder {
+    public static final class CompanyBuilder {
         //TODO: Implement Builder Pattern
+        private static int maxId = 1;
+        private int id;
+
+        private String companyname;
+        private String country;
+
+        public CompanyBuilder withMaxId(int maxIdToUse) {
+            this.maxId = maxIdToUse;
+            return this;
+        }
+
+        public CompanyBuilder withId(int idToUse) {
+            this.id = idToUse;
+            return this;
+        }
+
+        public CompanyBuilder withCompanyname(String companynameToUse) {
+            this.companyname = companynameToUse;
+            return this;
+        }
+
+        public CompanyBuilder withCountry(String countryToUse) {
+            this.country = countryToUse;
+            return this;
+        }
+
+        public Company build() {
+            return new Company(this);
+        }
     }
 
     public int getId() {
